01a2fd69acfb412829f78a5508c68343ee179f5a
pankun <rzpankun@126.com>
2020-06-22 17:31:17 +0800

    commit add application.xml

--- TestSpring/src/main/java/com/ktiuu/App.java
+++ TestSpring/src/main/java/com/ktiuu/App.java
@@ -2,6 +2,7 @@ package com.ktiuu;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Hello world!
@@ -9,8 +10,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App 
 {
+    String a;
     public static void main( String[] args ) {
 //        ClassPathXmlApplicationContext
-//        XmlBeanFactory
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("application.xml"));
+        App app = (App)xmlBeanFactory.getBean("app");
+        app = (App)xmlBeanFactory.getBean("app");
+        app.test();
     }
+
+
+    public void setA(String a){
+        this.a = a ;
+    }
+    public void test(){
+        System.out.println(this.a);
+    }
+
 }
