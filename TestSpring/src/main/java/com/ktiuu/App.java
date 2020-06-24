ade158c7b4f061d2a3977dfc52525250aa817ac5
pankun <rzpankun@126.com>
2020-06-24 15:47:06 +0800

    commit add App.java

--- TestSpring/src/main/java/com/ktiuu/App.java
+++ TestSpring/src/main/java/com/ktiuu/App.java
@@ -1,6 +1,10 @@
 package com.ktiuu;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
@@ -8,18 +12,26 @@ import org.springframework.core.io.ClassPathResource;
  * Hello world!
  *
  */
+@Configuration
 public class App 
 {
     String a;
     public static void main( String[] args ) {
-//        ClassPathXmlApplicationContext
-        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("application.xml"));
-        App app = (App)xmlBeanFactory.getBean("app");
-        app = (App)xmlBeanFactory.getBean("app");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
+//        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("application.xml"));
+//        App app = (App)xmlBeanFactory.getBean("app");
+//       ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+        App app = (App)applicationContext.getBean("app");
         app.test();
     }
 
 
+    @Bean(name = "app1")
+    public App app(){
+        App app = new App();
+        app.setA("pankun");
+        return app;
+    }
     public void setA(String a){
         this.a = a ;
     }
